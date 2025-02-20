9826b0bcf2a08db09e5b7658b18e8f1f8c13425b
steiva <steiva@uw.edu>
2025-02-19 16:47:14 -0800

    Add cuboid manipulation capabilities

--- configs/paths.py
+++ configs/paths.py
@@ -2,5 +2,4 @@ from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-CALIBRATION_PATH = os.path.join(BASE_DIR, 'configs', 'calibration.json')
-CAMERA_INTRINSICS_PATH = os.path.join(BASE_DIR, 'configs', 'camera_intrinsics.json')
+PROFILES_DIR = os.path.join(BASE_DIR, 'configs')
--- microtissue_manipulator/utils.py
+++ microtissue_manipulator/utils.py
@@ -8,19 +8,28 @@ class ManualRobotMovement:
     def __init__(self, openapi, block_thread = False):
         self.openapi = openapi
         self.positions = []
-        self.possible_steps = [0.1, 0.5, 1, 5, 10, 30, 50]
+        self.possible_steps = [0.01, 0.1, 0.5, 1, 3, 5, 10, 30, 50]
         self.step = 1
 
         # Register the key bindings
-        keyboard.on_press_key('up', lambda _: self.move_forward())
-        keyboard.on_press_key('down', lambda _: self.move_backward())
-        keyboard.on_press_key('left', lambda _: self.move_left())
-        keyboard.on_press_key('right', lambda _: self.move_right())
-        keyboard.on_press_key('pagedown', lambda _: self.move_z_down())
-        keyboard.on_press_key('pageup', lambda _: self.move_z_up())
-        keyboard.on_press_key('+', lambda _: self.increase_step())
-        keyboard.on_press_key('-', lambda _: self.decrease_step())
-        keyboard.on_press_key('s', lambda _: self.save_position())
+        # keyboard.on_press_key('up', lambda _: self.move_forward())
+        # keyboard.on_press_key('down', lambda _: self.move_backward())
+        # keyboard.on_press_key('left', lambda _: self.move_left())
+        # keyboard.on_press_key('right', lambda _: self.move_right())
+        # keyboard.on_press_key('pagedown', lambda _: self.move_z_down())
+        # keyboard.on_press_key('pageup', lambda _: self.move_z_up())
+        # keyboard.on_press_key('+', lambda _: self.increase_step())
+        # keyboard.on_press_key('-', lambda _: self.decrease_step())
+        # keyboard.on_press_key('s', lambda _: self.save_position())
+        keyboard.add_hotkey('up', self.move_forward)
+        keyboard.add_hotkey('down', self.move_backward)
+        keyboard.add_hotkey('left', self.move_left)
+        keyboard.add_hotkey('right', self.move_right)
+        keyboard.add_hotkey('pagedown', self.move_z_down)
+        keyboard.add_hotkey('pageup', self.move_z_up)
+        keyboard.add_hotkey('+', self.increase_step)
+        keyboard.add_hotkey('-', self.decrease_step)
+        keyboard.add_hotkey('s', self.save_position)
         # Keep the program running until 'q' is pressed
         if block_thread:
             keyboard.wait('q')
@@ -38,37 +47,37 @@ class ManualRobotMovement:
         return can_move
 
     def move_z_down(self):
-        x,y,z = self.openapi.get_position(verbose = False).values()
+        x,y,z = self.openapi.get_position(verbose = False)[0].values()
         potential_position = (x, y, z-self.step)
         if self.position_safeguard(potential_position):
             self.openapi.move_relative('z', -self.step)
 
     def move_z_up(self):
-        x,y,z = self.openapi.get_position(verbose = False).values()
+        x,y,z = self.openapi.get_position(verbose = False)[0].values()
         potential_position = (x, y, z+self.step)
         if self.position_safeguard(potential_position):
             self.openapi.move_relative('z', self.step)
 
     def move_forward(self):
-        x,y,z = self.openapi.get_position(verbose = False).values()
+        x,y,z = self.openapi.get_position(verbose = False)[0].values()
         potential_position = (x, y+self.step, z)
         if self.position_safeguard(potential_position):
             self.openapi.move_relative('y', self.step)
 
     def move_backward(self):
-        x,y,z = self.openapi.get_position(verbose = False).values()
+        x,y,z = self.openapi.get_position(verbose = False)[0].values()
         potential_position = (x, y-self.step, z)
         if self.position_safeguard(potential_position):
             self.openapi.move_relative('y', -self.step)
 
     def move_left(self):
-        x,y,z = self.openapi.get_position(verbose = False).values()
+        x,y,z = self.openapi.get_position(verbose = False)[0].values()
         potential_position = (x-self.step, y, z)
         if self.position_safeguard(potential_position):
             self.openapi.move_relative('x', -self.step)
 
     def move_right(self):
-        x,y,z = self.openapi.get_position(verbose = False).values()
+        x,y,z = self.openapi.get_position(verbose = False)[0].values()
         potential_position = (x+self.step, y, z)
         if self.position_safeguard(potential_position):
             self.openapi.move_relative('x', self.step)
@@ -88,12 +97,22 @@ class ManualRobotMovement:
             self.step = self.possible_steps[current_index - 1]
 
     def save_position(self):
-        position = self.openapi.get_position(verbose = False)
+        position = self.openapi.get_position(verbose = False)[0]
         self.positions.append((position['x'], position['y'], position['z']))
         print(f"Saved position: {position}")
 
-def check_calibration_config():
-    config_path = paths.CALIBRATION_PATH
+def create_configuration_profile(profile_name):
+    profile_path = os.path.join(paths.PROFILES_DIR, profile_name)
+    if not os.path.exists(profile_path):
+        os.makedirs(profile_path)
+        check_calibration_config(profile_name)
+        check_camera_config(profile_name)
+        print(f"Profile '{profile_name}' created at {profile_path}.")
+    else:
+        print(f"Profile '{profile_name}' already exists at {profile_path}.")
+
+def check_calibration_config(profile_name):
+    config_path = os.path.join(paths.PROFILES_DIR, profile_name, 'calibration.json')
     if os.path.exists(config_path):
         print("calibration.json file exists.")
     else:
@@ -102,14 +121,28 @@ def check_calibration_config():
             "robot_coords": [],  # List of robot coordinates
             "camera_coords": [],  # List of camera coordinates
             "tf_mtx": [],  # Transformation matrix
-            "calib_origin": []  # Calibration origin
+            "calib_origin": [],  # Calibration origin
+            "offset": [],
+            "size_conversion_ratio": 1.0,
+            "one_d_ratio": 1.0
         }
         with open(config_path, 'w') as json_file:
             json.dump(data, json_file, indent=4)
         print("calibration.json file created.")
 
-def check_camera_config():
-    config_path = paths.CAMERA_INTRINSICS_PATH
+def load_calibration_config(profile_name):
+    config_path = os.path.join(paths.PROFILES_DIR, profile_name, 'calibration.json')
+    with open(config_path, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+def save_calibration_config(profile_name, data):
+    config_path = os.path.join(paths.PROFILES_DIR, profile_name, 'calibration.json')
+    with open(config_path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+def check_camera_config(profile_name):
+    config_path = os.path.join(paths.PROFILES_DIR, profile_name, 'camera_intrinsics.json')
     if os.path.exists(config_path):
         print("camera_intrinsics.json file exists.")
     else:
